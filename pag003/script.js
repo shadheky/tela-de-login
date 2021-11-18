0995c7e724316f828b66678da75ef0758ae36d29
Luiz <ll8259875@gmail.com>
2021-11-18 01:41:56 -0300

    first commit

---
+++ pag003/script.js
@@ -0,0 +1,7 @@
+function clicar(){
+var res = document.querySelector('p#p1')
+var input1 = document.querySelector('input#ipnt1')
+if(input1.value==0){alert('Cheque os dados e tente novamente')}else{res.innerHTML ='Recebemos o seu pedido, se este email esta vinculado a alguma conta você recebera uma solicitação de troca de senha !!'}
+
+
+}
